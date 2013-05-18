a392641d5a4251f0407313565cacb976bae204d4
calzonez <jinho.c.c@gmail.com>
2013-05-18 01:37:16 -0400

    working on modulizing the framework

--- controllers/test.php
+++ controllers/test.php
@@ -33,4 +33,26 @@ class Test_Controller extends Base
 		$this->load_view('test/testing', array('test'=>1));
      
     }
+    
+    public function aa()
+    {
+    	/*
+    	 * this is hypothetical.
+    	 */
+    	
+    	$this->load_module('hypothetical_module'); //maybe do this in.. core/base constructor?
+
+    	/*
+    	 * router should first check local files when redirecting.
+    	 * no file exist?
+    	 * look for included file in module.
+    	 * if the module exist, redirect to the module.
+    	 * module's db config is overwritten by the parent module.
+    	 * css files always point to the parent module's style
+    	 * module's db structure is always important
+    	 * modules may contain config vars.. that need to be configured
+    	 * 
+    	 */
+    	
+    }
 }
